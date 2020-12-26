20197ae9a7a5aa4d85383ca26a2ff5689f238f74
oskardrums <moskar.drummer@gmail.com>
2020-12-27 00:33:28 +0200

    clean setup.py

--- hw2/setup.py
+++ hw2/setup.py
@@ -1,13 +1,9 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
+
 setup(
     name='mykmeanspp',
     version='0.1.0',
     ext_modules=[
-        Extension(
-            # the qualified name of the extension module to build
-            'mykmeanspp',
-            # the files to compile into our module relative to ``setup.py``
-            ['kmeans.c'],
-        ),
+        Extension('mykmeanspp', ['kmeans.c']),
     ]
 )
