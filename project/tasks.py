effb521098fb8a956366b108317993712f0d7e6c
Oskar Mazerath <moskar.drummer@gmail.com>
2021-04-17 15:02:54 +0300

    use python 3.8.5

--- project/tasks.py
+++ project/tasks.py
@@ -2,11 +2,11 @@ from invoke import task, call
 
 @task
 def run(c, n=0, k=0, Random=True):
-    c.run("python3 setup.py build_ext --inplace")
+    c.run("python3.8.5 setup.py build_ext --inplace")
     if Random:
-        c.run("python3 main.py -r", pty=True)
+        c.run("python3.8.5 main.py -r", pty=True)
     else:
-        c.run("python3 main.py -n %lu -k %lu" % (n, k), pty=True)
+        c.run("python3.8.5 main.py -n %lu -k %lu" % (n, k), pty=True)
 
 
 @task(aliases=['del'])
